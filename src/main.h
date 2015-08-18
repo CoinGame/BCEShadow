151f41429f825cf4527595fd4943e1b5aea39de3
Michael Witrant <michael@ouvrages-web.fr>
2015-08-18 07:32:18 +0200

    Fix block vote serialization problem

--- src/main.h
+++ src/main.h
@@ -1627,7 +1627,7 @@ public:
         if (nVersion >= PROTOCOL_V2_0)
             READWRITE(nProtocolVersion);
         else if (fRead)
-            const_cast<CDiskBlockIndex*>(this)->nProtocolVersion = 0;
+            const_cast<CDiskBlockIndex*>(this)->nProtocolVersion = PROTOCOL_V2_0;
 
         READWRITE(hashNext);
         READWRITE(nFile);
@@ -1657,7 +1657,7 @@ public:
             READWRITE(prevoutStake);
             READWRITE(nStakeTime);
             READWRITE(hashProofOfStake);
-            READWRITE(vote);
+            nSerSize += SerReadWrite(s, vote, nType, nProtocolVersion, ser_action);
             READWRITE(vParkRateResult);
             READWRITE(nCoinAgeDestroyed);
             READWRITE(vElectedCustodian);
