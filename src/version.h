d8a67c726d9d73282856c991669d993fd2447a37
Michael Witrant <michael@ouvrages-web.fr>
2015-09-07 12:23:25 +0200

    Version 4.0.0

--- src/version.h
+++ src/version.h
@@ -17,8 +17,8 @@
 // These need to be macro's, as version.cpp's voodoo requires it
 
 // nu version
-#define CLIENT_VERSION_MAJOR       3
-#define CLIENT_VERSION_MINOR       1
+#define CLIENT_VERSION_MAJOR       4
+#define CLIENT_VERSION_MINOR       0
 #define CLIENT_VERSION_REVISION    0
 
 static const int CLIENT_VERSION =
